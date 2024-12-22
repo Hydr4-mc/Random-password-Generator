5d3f5630609e107e8f36dd768005a1ec3c2d060b
Hydr4-mc <owensmeyer1@gmail.com>
2024-12-21 19:52:10 -0600

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,7 +1,8 @@
 import random
 import string
 
-size_of_pasword = 10
+size_of_pasword = int(input("how long do you want the password?"))
 posible_digits = string.ascii_lowercase + string.ascii_uppercase + string.digits + "!" + "@" + "#" + "$" + "%" + "^" + "&" + "*" + "?"
 
 print("".join(random.choices(posible_digits, k=size_of_pasword)))
+useless=str(input())
